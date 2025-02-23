a81e6d186ec8d098fcd4a1ff592585857f568538
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2025-02-23 13:51:58 -0300

    feat: created Models and Pages for Login and Register

---
+++ TechLibrary.Frontend/Models/RegisterModel.cs
@@ -0,0 +1,8 @@
+namespace TechLibrary.Frontend.Models;
+
+public class RegisterModel
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
